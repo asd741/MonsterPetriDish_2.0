d07f5fe8ac791f0137e374fdac8f9a7f879bf25a
MinNingTong <a22833593@gmail.com>
2019-04-05 20:30:48 +0800

    部署設定完成

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,6 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/asd741/Documents/projects/MonsterPetriDish_2.0/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/Users/asd741/Documents/projects/MonsterPetriDish_2.0/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-download-js": () => import("/Users/asd741/Documents/projects/MonsterPetriDish_2.0/src/pages/download.js" /* webpackChunkName: "component---src-pages-download-js" */),
   "component---src-pages-index-js": () => import("/Users/asd741/Documents/projects/MonsterPetriDish_2.0/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
