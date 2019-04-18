5f2b2074b3054c7fc4478a1b9fc3150d8d6596e1
asd741 <a22833593@gmail.com>
2019-04-18 22:59:53 +0800

    '準備處理穿牆bug'

--- src/components/story/book.js
+++ src/components/story/book.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./book.sass";
 const Book = () => {
+  let alldegs=[];
   useEffect(() => {//自適應頁數增減
     let aPage = document.getElementsByClassName('page'),
       aMidPage = document.getElementsByClassName('mid-page'),
@@ -23,18 +24,19 @@ const Book = () => {
       thatdeg,
       oWrap = document.getElementsByClassName('book-page-wrapper')[0],
       handleTransition = () => {
-        if (vX && that && thatdeg && Math.abs(vX) > 0.1) {
-          thatdeg=thatdeg - vX;
+        if (vX && that && thatdeg && Math.abs(vX) >= 0.1 && thatdeg > -180 && thatdeg <= 0) {
+          thatdeg = thatdeg - (vX * 0.2);
           that.style.transform = `rotateY(${thatdeg}deg`;
-          vX *= 0.95;
-          console.log('vX', vX,'thatdeg',thatdeg);
+          Math.abs(Math.abs(thatdeg) - 90) < 20 ? vX *= 1.05 : vX *= 0.95;//兩個abs是解決90度左右很難翻頁的問題，然後*=是做受力運動
           requestAnimationFrame(handleTransition);
+        }else{
+          console.log(alldegs);
         }
       }
     oWrap.onmousemove = e => {
-      vX = (sX - e.clientX) / 2;
+      vX = (sX - e.clientX) / 4;
       thatdeg = parseInt(that.style.transform.match(/-?\d+/)[0]);
-      if ((thatdeg - vX) < -180 || (thatdeg - vX) > 0) {
+      if ((thatdeg - vX) <= -180 || (thatdeg - vX) >= 0) {
         thatdeg = Math.max(-180, thatdeg);
         thatdeg = Math.min(0, thatdeg);
         that.style.transform = `rotateY(${thatdeg}deg`;
@@ -46,6 +48,7 @@ const Book = () => {
     oWrap.onmouseup = () => {
       handleTransition();
       oWrap.onmousemove = null;
+      console.log(alldegs);
     }
   }
 
