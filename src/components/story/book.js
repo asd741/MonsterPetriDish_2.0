3ee85f55478464691485488c53eed497beb0866a
asd741 <a22833593@gmail.com>
2019-04-20 12:22:36 +0800

    '角度初始化優化'

--- src/components/story/book.js
+++ src/components/story/book.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./book.sass";
 const Book = () => {
-  let aPage, aPageDegs = [], gap;
+  let aPage, aPageDegs = [], gap, space = 25;
   useEffect(() => {//自適應頁數增減
-    let aMidPage = document.getElementsByClassName('mid-page'), space = 25;
-    gap = space / (aMidPage.length + 1);//+1是首頁 因為首頁也要分到角度
+    // let aMidPage = document.getElementsByClassName('mid-page'), space = 25;
+    // gap = space / (aMidPage.length + 1);//+1是首頁 因為首頁也要分到角度
+    // aPage = document.getElementsByClassName('page');
+    // aPage[0].style = `transform: rotateY(${-space}deg)`;//封面
+    // aPage[aPage.length - 1].style = `transform: rotateY(0deg)`;//底頁
+    // [].forEach.call(aMidPage, (item, index) => {
+    //   const d = (aMidPage.length - index) * gap;
+    //   item.style = `transform: rotateY(${-d}deg);z-index:${aMidPage.length - index}`
+    // })
     aPage = document.getElementsByClassName('page');
-    aPage[0].style = `transform: rotateY(${-space}deg)`;//封面
-    aPage[aPage.length - 1].style = `transform: rotateY(0deg)`;//底頁
-    [].forEach.call(aMidPage, (item, index) => {
-      const d = (aMidPage.length - index) * gap;
-      item.style = `transform: rotateY(${-d}deg);z-index:${aMidPage.length - index}`
+    // 假設共5頁 =>25 18 12 6 0，25度分給4個對象
+    gap = space / (aPage.length - 1);//最後一頁是0度、其他均分度數
+    [].forEach.call(aPage, (item, index) => {
+      item.style.zIndex = aPage.length - index;
+      //length=5  index=01234  gap*4  gap*3 gap*2 gap*1 gap*0
+      const d = -(aPage.length - 1 - index) * gap;
+      item.style.transform = `rotateY(${d}deg)`;
     })
     window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
       window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
@@ -22,18 +31,26 @@ const Book = () => {
       sX = e.clientX,
       vX,
       thatdeg,
+      leftBeyond = aPageDegs[index - 1] || -180,
+      rightBeyond = aPageDegs[index + 1] || 0,
       oWrap = document.getElementsByClassName('book-page-wrapper')[0],
-      penetrateJudge = () => {
-        if (Math.abs(thatdeg) + gap >= Math.abs(aPageDegs[index - 1]) || Math.abs(thatdeg) + gap <= Math.abs(aPageDegs[index + 1])) {
-          that.style.transform = `rotateY(${aPageDegs[index - 1]+gap}deg`;
-          console.log('摸到隔壁的紙了');
+      handleBeyond = () => {//判斷是否摸到隔壁的紙
+        console.log(leftBeyond, thatdeg, rightBeyond);
+
+        if (thatdeg < leftBeyond || thatdeg > rightBeyond) {
+          that.style.transform = `rotateY(${thatdeg}deg`;
+          aPageDegs[index] = thatdeg;//記錄該頁的角度
           return true;
         }
+        // if (Math.abs(thatdeg) + gap >= Math.abs(aPageDegs[index - 1]) || Math.abs(thatdeg) + gap <= Math.abs(aPageDegs[index + 1])) {
+        //   // that.style.transform = `rotateY(${aPageDegs[index - 1]+gap}deg`;
+        //   // console.log('摸到隔壁的紙了');
+        //   return true;
+        // }
       },
       handleTransition = () => {
-
         if (vX && that && thatdeg && Math.abs(vX) >= 0.1 && thatdeg > -180 && thatdeg <= 0) {
-          if (penetrateJudge() === true) {
+          if (handleBeyond() === true) {
             return;
           }
           thatdeg = thatdeg - (vX * 0.2);
@@ -43,20 +60,13 @@ const Book = () => {
           aPageDegs[index] = thatdeg;
         }
       }
-      console.log(index,aPageDegs);
     oWrap.onmousemove = e => {
       vX = (sX - e.clientX) / 4;
       thatdeg = parseInt(that.style.transform.match(/-?\d+/)[0]);
-      if (penetrateJudge() === true) {
+      if (handleBeyond() === true) {
         return;
       }
-      if ((thatdeg - vX) <= -180 || (thatdeg - vX) >= 0) {
-        thatdeg = Math.max(-180, thatdeg);
-        thatdeg = Math.min(0, thatdeg);
-        that.style.transform = `rotateY(${thatdeg}deg`;
-      } else {
-        that.style.transform = `rotateY(${thatdeg - vX}deg`;
-      }
+
       aPageDegs[index] = thatdeg;
 
       sX = e.clientX;
